66c7d4ff23e005de584981b2cd029466c60841db
Mario Andre <mario.andre.m@gmail.com>
2023-01-21 11:39:21 -0300

    regex to get links from markdown files and async logic

--- compiled/index.js
+++ compiled/index.js
@@ -1,2 +1,43 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import chalk from "chalk";
-console.log(chalk.blue("hello"));
+import { readFile } from "fs/promises";
+const errorTreatment = (error) => {
+    throw new Error(chalk.red(error.message));
+};
+const showsContent = (content) => {
+    console.log(chalk.green(content));
+};
+const linkExtractor = (text) => {
+    const regex = /\[([^[\]]*?)]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
+    const matches = [...text.matchAll(regex)].map((link) => ({
+        title: link[1],
+        url: link[2],
+    }));
+    return matches;
+};
+const getFile = (path) => __awaiter(void 0, void 0, void 0, function* () {
+    const encoding = "utf-8";
+    try {
+        const text = yield readFile(path, encoding);
+        showsContent(text);
+        const links = linkExtractor(text);
+        links.map((item) => console.log(chalk.blue(`${item.title} - ${item.url}`)));
+    }
+    catch (error) {
+        if (typeof error === "object") {
+            errorTreatment(error);
+        }
+        else {
+            console.error(error);
+        }
+    }
+});
+getFile("./files/texto.md");
--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,50 @@
-// antigamente
-//  const chalk = require('chalk')
+// antigamente -> const chalk = require('chalk')
 import chalk from "chalk";
+import { readFile } from "fs/promises";
 
-console.log(chalk.blue("hello"));
+interface LinkInfoObject {
+  title: string;
+  url: string;
+}
+
+const errorTreatment = (error: NodeJS.ErrnoException) => {
+  throw new Error(chalk.red(error.message));
+};
+
+const showsContent = (content: string) => {
+  console.log(chalk.green(content));
+};
+
+const linkExtractor = (text: string) => {
+  const regex = /\[([^[\]]*?)]\((https?:\/\/[^\s?#.]*[^\s]*)\)/gm;
+  const matches: Array<LinkInfoObject> = [...text.matchAll(regex)].map(
+    (link) => ({
+      title: link[1],
+      url: link[2],
+    })
+  );
+  return matches;
+};
+
+// const getFile = (path: string) => {
+//   const encoding = "utf-8";
+//   readFile(path, encoding).then(showsContent).catch(errorTreatment);
+// };
+
+const getFile = async (path: string) => {
+  const encoding = "utf-8";
+  try {
+    const text = await readFile(path, encoding);
+    showsContent(text);
+    const links = linkExtractor(text);
+    links.map((item) => console.log(chalk.blue(`${item.title} - ${item.url}`)));
+  } catch (error: any) {
+    if (typeof error === "object") {
+      errorTreatment(error);
+    } else {
+      console.error(error);
+    }
+  }
+};
+
+getFile("./files/texto.md");
